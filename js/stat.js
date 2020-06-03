f4e64ea5d46d1dbbe4ab20bb60a37fb240b3d12f
Depressia <cray86@mail.ru>
2020-06-04 02:27:55 +0300

    Добавляет вывод статистики прохождения игры.

--- js/stat.js
+++ js/stat.js
@@ -1,42 +1,64 @@
 'use strict';
 
-var CLOUD_WIDTH = 500;
-var CLOUD_HEIGHT = 200;
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
-var CLOUD_Y = 50;
-var GAP = 10;
-var FONT_GAP = 15;
-var TEXT_WIDTH = 50;
-var BAR_HEIGHT = 20;
-var barWidth = CLOUD_WIDTH - GAP - TEXT_WIDTH - GAP;
+var CLOUD_Y = 10;
+
+var BAR_WIDTH = 40;
+var BAR_HEIGTH = -150;
+var BAR_GAP = 50;
+
+var FONT_GAP = 10;
+
+var BAR_START_X = CLOUD_X + 40;
+var BAR_START_Y = CLOUD_Y + 235;
+
+var NAME_START_X = BAR_START_X;
+var NAME_START_Y = BAR_START_Y + FONT_GAP;
 
 var renderCloud = function(ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function(arr) {
-  var maxElement = arr[0];
+var getMaxTime = function(time) {
+  var maxElement = time[0];
 
-  for (var i = 1; i < arr.length; i++) {
-    if (arr[i] > maxElement) {
-      maxElement = arr[i];
+  for (var i = 1; i < time.length; i++) {
+    if (time[i] > maxElement) {
+      maxElement = time[i];
     }
   }
 
   return maxElement;
 };
 
-window.renderStatistics = function(ctx, players, times) {
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
+window.renderStatistics = function (ctx, players, times) {
+  renderCloud(ctx, CLOUD_X + 10, CLOUD_Y + 10, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
   ctx.fillStyle = '#000';
+  ctx.font = '16px Pt Mono';
+  ctx.textBaseline = 'hanging';
+  ctx.fillText('Ура вы победили!', CLOUD_X + 20, CLOUD_Y + 20);
+  ctx.fillText('Список результатов:', CLOUD_X + 20, CLOUD_Y + 40);
 
-  var maxTime = getMaxElement(times);
+  var maxTime = getMaxTime(times);
 
   for (var i = 0; i < players.length; i++) {
-    ctx.fillText(players[i], CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP + (GAP + BAR_HEIGHT) * i);
-    ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, CLOUD_Y + GAP + (GAP + BAR_HEIGHT) * i, (barWidth * times[i]) / maxTime, BAR_HEIGHT);
+    var saturate = 100 * Math.random();
+    var playerTime = Math.ceil(times[i]);
+    console.log('Время игрока ' + players[i] + ' составляет ' + playerTime);
+
+    ctx.fillStyle = '#000';
+    ctx.fillText(players[i], NAME_START_X + (BAR_WIDTH + BAR_GAP) * i, NAME_START_Y);
+    ctx.fillText(playerTime, BAR_START_X + (BAR_WIDTH + BAR_GAP) * i, BAR_START_Y - 2 * FONT_GAP + (BAR_HEIGTH * times[i]) / maxTime);
+    if (players[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = 'hsl(240, '+ saturate +'%, 50%)';
+    }
+    ctx.fillRect(BAR_START_X + (BAR_WIDTH + BAR_GAP) * i, BAR_START_Y, BAR_WIDTH, (BAR_HEIGTH * times[i]) / maxTime);
   }
 };
