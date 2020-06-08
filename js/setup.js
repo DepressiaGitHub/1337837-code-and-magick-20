90e1f468ba610b15dff133c99bbb790975886e69
Depressia <cray86@mail.ru>
2020-06-08 23:53:47 +0300

    Убираем лишний параметр в функции по созданию данных мага.

--- js/setup.js
+++ js/setup.js
@@ -24,17 +24,16 @@ var getRandom = function (start, end) {
   return Math.round(random);
 };
 
-var getWizards = function (createWizard) {
+var getWizards = function () {
   var wizardName = WIZARD_NAMES[getRandom(0, WIZARD_NAMES.length - 1)] + ' ' + WIZARD_SURNAMES[getRandom(0, WIZARD_SURNAMES.length - 1)];
   var wizardCoatColor = WIZARD_COAT_COLOR[getRandom(0, WIZARD_COAT_COLOR.length - 1)];
   var wizardEyesColor = WIZARD_EYES_COLOR[getRandom(0, WIZARD_EYES_COLOR.length - 1)];
-  createWizard = {
+
+  return {
     name: wizardName,
     coatColor: wizardCoatColor,
     eyesColor: wizardEyesColor
   };
-
-  return createWizard;
 };
 
 for (var i = 0; i < 4; i++) {
