77d5e37662a34bc94c6473d8445cef5f29da47fc
Depressia <cray86@mail.ru>
2020-07-08 08:28:42 +0300

    Загружает данные с сервера и отправляет форму.

---
+++ js/backend.js
@@ -0,0 +1,53 @@
+'use strict';
+
+(function () {
+  var URL_GET = 'https://javascript.pages.academy/code-and-magick/data';
+  var URL_POST = 'https://javascript.pages.academy/code-and-magick';
+  var TIMEOUT_IN_MS = 10000;
+  var StatusCode = {
+    OK: 200
+  };
+
+  var load = function (onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === StatusCode.OK) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения.');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс.');
+    });
+
+    xhr.timeout = TIMEOUT_IN_MS;
+
+    xhr.open('GET', URL_GET);
+    xhr.send();
+  };
+
+  var save = function (data, onLoad) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      onLoad(xhr.response);
+    });
+
+    xhr.open('POST', URL_POST);
+    xhr.send(data);
+  };
+
+  window.backend = {
+    load: load,
+    save: save
+  };
+})();
--- js/setup.js
+++ js/setup.js
@@ -1,70 +1,70 @@
 'use strict';
 
-var userDialog = document.querySelector('.setup');
-var similarListElement = userDialog.querySelector('.setup-similar-list');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template')
-  .content
-  .querySelector('.setup-similar-item');
+(function () {
+  var userDialog = document.querySelector('.setup');
+  var similarListElement = userDialog.querySelector('.setup-similar-list');
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
 
-var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
-var WIZARD_FIREBALL_COLOR = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
-var wizards = [];
+  var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+  var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+  var WIZARD_FIREBALL_COLOR = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+  var MAX_WIZARD_COUNT = 4;
 
-var getWizards = function () {
-  var wizardName = window.util.getRandomElement(WIZARD_NAMES) + ' ' + window.util.getRandomElement(WIZARD_SURNAMES);
-  var wizardCoatColor = window.util.getRandomElement(WIZARD_COAT_COLOR);
-  var wizardEyesColor = window.util.getRandomElement(WIZARD_EYES_COLOR);
+  var renderWizard = function (wizard) {
+    var wizardElement = similarWizardTemplate.cloneNode(true);
 
-  return {
-    name: wizardName,
-    coatColor: wizardCoatColor,
-    eyesColor: wizardEyesColor
+    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
+
+    return wizardElement;
   };
-};
 
-for (var i = 0; i < 4; i++) {
-  wizards[i] = getWizards();
-}
-
-var renderWizard = function (wizard) {
-  var wizardElement = similarWizardTemplate.cloneNode(true);
-
-  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
-
-  return wizardElement;
-};
-
-var fragment = document.createDocumentFragment();
-
-for (var j = 0; j < wizards.length; j++) {
-  fragment.appendChild(renderWizard(wizards[j]));
-}
-
-similarListElement.appendChild(fragment);
-
-userDialog.querySelector('.setup-similar').classList.remove('hidden');
+  // Переменные для изменения цвета при клике.
+  var setupPlayer = document.querySelector('.setup-player');
+  var setupPlayerCoat = setupPlayer.querySelector('.wizard-coat');
+  var setupPlayerEyes = setupPlayer.querySelector('.wizard-eyes');
+  var setupPlayerFireball = setupPlayer.querySelector('.setup-fireball-wrap');
 
 
-// Переменные для изменения цвета при клике.
-var setupPlayer = document.querySelector('.setup-player');
-var setupPlayerCoat = setupPlayer.querySelector('.wizard-coat');
-var setupPlayerEyes = setupPlayer.querySelector('.wizard-eyes');
-var setupPlayerFireball = setupPlayer.querySelector('.setup-fireball-wrap');
+  setupPlayerCoat.addEventListener('click', function () {
+    window.colorize(setupPlayerCoat, WIZARD_COAT_COLOR, 'coat-color');
+  });
 
+  setupPlayerEyes.addEventListener('click', function () {
+    window.colorize(setupPlayerEyes, WIZARD_EYES_COLOR, 'eyes-color');
+  });
 
-setupPlayerCoat.addEventListener('click', function () {
-  window.colorize(setupPlayerCoat, WIZARD_COAT_COLOR, 'coat-color');
-});
+  setupPlayerFireball.addEventListener('click', function () {
+    window.colorize(setupPlayerFireball, WIZARD_FIREBALL_COLOR, 'fireball-color');
+  });
 
-setupPlayerEyes.addEventListener('click', function () {
-  window.colorize(setupPlayerEyes, WIZARD_EYES_COLOR, 'eyes-color');
-});
+  // Усешно загружаем магов по данным с сервера.
+  var successHandler = function (wizards) {
+    var fragment = document.createDocumentFragment();
 
-setupPlayerFireball.addEventListener('click', function () {
-  window.colorize(setupPlayerFireball, WIZARD_FIREBALL_COLOR, 'fireball-color');
-});
+    for (var i = 0; i < MAX_WIZARD_COUNT; i++) {
+      fragment.appendChild(renderWizard(wizards[i]));
+    }
+    similarListElement.appendChild(fragment);
+
+    userDialog.querySelector('.setup-similar').classList.remove('hidden');
+  };
+
+  // Ошибка загрузки данных с магами.
+  var errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 9999; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '40px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  window.backend.load(successHandler, errorHandler);
+})();
